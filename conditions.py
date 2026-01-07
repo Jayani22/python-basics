340ace6139e60bc4a6aeae7bb3e52d10479bc2f5
Jayani22 <mutcherlajayani@gmail.com>
2026-01-07 12:13:48 +0530

    python

--- conditions.py
+++ conditions.py
@@ -5,8 +5,9 @@ change_ticket = False
 ennvironment = ennvironment.casefold()
 if ennvironment == "prod":
     change_ticket = True
-    print("Please provide a change ticket:")
+    print("Please provide a change ticket")
 elif ennvironment == "staging":
     print("Welcome to staging environment")
 else:
     print("Please login using your credentials")
+ 
