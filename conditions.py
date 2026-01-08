0da83e0b21177aca2e863f920cf2b87bddd20429
Jayani22 <mutcherlajayani@gmail.com>
2026-01-08 10:22:13 +0530

    python

--- conditions.py
+++ conditions.py
@@ -1,13 +1,14 @@
 # python is case-sensitive i.e., 'a' and 'A' are different
-ennvironment = input("Enter your environment: ")
+environment = input("Enter your environment: ")
+print(type(environment))
 change_ticket = False
 
-ennvironment = ennvironment.casefold()
-if ennvironment == "prod":
+environment = environment.casefold()
+
+if environment == "prod":
     change_ticket = True
     print("Please provide a change ticket")
-elif ennvironment == "staging":
+elif environment == "staging":
     print("Welcome to staging environment")
 else:
     print("Please login using your credentials")
- 
